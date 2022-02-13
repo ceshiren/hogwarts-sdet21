dbb2c552c0ec0ef7085d62c600cbd9f34fcf2c20
seveniruby <seveniruby@gmail.com>
2022-02-13 16:39:58 +0800

    更新脚本 追加依赖清单

--- wechat_web_test/test_ceshiren.py
+++ wechat_web_test/test_ceshiren.py
@@ -6,12 +6,15 @@ from selenium.webdriver.common.by import By
 
 
 class TestCeshiren:
-    def setup(self):
-        self.driver=webdriver.Firefox()
+    def setup_class(self):
+        self.driver = webdriver.Firefox()
         self.driver.get("https://ceshiren.com")
         self.driver.implicitly_wait(10)
 
     def test_search(self):
-        self.driver.find_element(By.ID,  'search-button').click()
-        self.driver.find_element(By.ID, 'search-term').send_keys("python 21"+Keys.ENTER)
+        self.driver.find_element(By.ID, 'search-button').click()
+        self.driver.find_element(By.ID, 'search-term').send_keys("python 21" + Keys.ENTER)
         self.driver.find_element(By.CSS_SELECTOR, '[title=打开高级搜索]').click()
+
+    def teardown_class(self):
+        self.driver.quit()
